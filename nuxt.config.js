a86fe925f3b94faa3a7640a6eddceca43c013d12
ed85w <ed.walker@hotmail.co.uk>
2020-09-15 21:21:19 +0100

    github pages deploy test

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,9 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'static',
+  router: {
+    base: '/tiny-stompers-nuxt/'
+  },
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
