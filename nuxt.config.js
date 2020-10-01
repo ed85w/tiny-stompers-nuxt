13f1d7dc2875e5d5359530b058de2bf137de4dbb
ed85w <ed.walker@hotmail.co.uk>
2020-10-01 18:06:42 +0100

    facebook sdk working in dev

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
       { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'}
     ],
     script: [
-      { src: '/js/fb-sdk.js' }
+      { src: './js/fb-sdk.js' }
      ]
   },
   /*
