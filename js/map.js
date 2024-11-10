ef3a2a2d5d5e37261aa362834589f3c35d74c5e8
ed85w <ed.walker@hotmail.co.uk>
2024-11-10 17:11:53 +0000

    5f86a34

---
+++ js/map.js
@@ -0,0 +1,32 @@
+// Initialize and add the map
+function initMap() {
+
+  // The location of tinyStompers
+  var tinyStompers = {lat: 54.233292, lng: -1.3316585};
+
+  // Map options
+  var options = {
+    zoom: 14,
+    center: tinyStompers,
+  }
+
+  // The map, centered at tinyStompers
+  var map = new google.maps.Map(
+      document.getElementById('map'), options);
+
+  // The icon
+  var icon = {
+    url: "./tiny-stompers-logo-pin.png", // url
+    scaledSize: new google.maps.Size(40, 40), // scaled size
+};
+
+  // The marker, positioned at tinyStompers
+  var marker = new google.maps.Marker({
+    position: tinyStompers, 
+    map: map,
+    icon: icon,
+  });
+
+
+   
+}
